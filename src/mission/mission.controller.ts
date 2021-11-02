3d215ab35e1c1c22f25aa607a7c30bd793eb39a0
sunrabbit123 <qudwls185@naver.com>
2021-11-02 18:06:07 +0900

    add : mission service

--- src/mission/mission.controller.ts
+++ src/mission/mission.controller.ts
@@ -1,4 +1,15 @@
-import { Controller, Delete, Get, Patch, Post } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Headers,
+} from "@nestjs/common";
+import { getSubByToken } from "../util/token";
+import { CreateMission, MissionType } from "./mission.dto";
 import { MissionService } from "./mission.service";
 
 @Controller("mission")
@@ -6,20 +17,40 @@ export class MissionController {
   constructor(private readonly missionService: MissionService) {}
 
   @Post("add") // 미션 생성
-  async addMission() {}
+  async addMission(@Body() body: CreateMission) {
+    return this.missionService.createMission(body);
+  }
 
-  @Delete(":type/:number") // 미션 삭제
-  async dropMission() {}
+  @Delete(":number") // 미션 삭제
+  async dropMission(@Param("number") number: number) {
+    return this.missionService.dropMission({ number });
+  }
 
-  @Get(":type/:number") // 타입과 숫자로 미션 조회
-  async getMissionByTypeAndNumber() {}
+  @Get(":number") // 숫자로 미션 조회
+  async getMissionByNumber(@Param("number") number: number) {
+    return this.missionService.getMissionByNumber(number);
+  }
 
-  @Get("list/:type") // 타입으로 미션 여러개 조회
-  async getMissionList() {}
+  @Get(":type") // 타입으로 미션들 조회
+  async getMissionListByType(@Param("type") type: MissionType) {
+    return this.missionService.getMissionListByType(type);
+  }
 
-  @Patch("clear/:type/:number") // 미션 성공시
-  async successMission() {}
+  @Patch("clear/:number") // 미션 성공시
+  async successMission(
+    @Headers("authorization") accessToken: string,
+    @Param("number") number: number
+  ) {
+    const sub = getSubByToken(accessToken);
+    return this.missionService.sucessMissionBySub({ sub, number });
+  }
 
-  @Patch("fail/:type/:number") // 미션 실패시
-  async failMission() {}
+  @Patch("fail/:number") // 미션 실패시
+  async failMission(
+    @Headers("authorization") accessToken: string,
+    @Param("number") number: number
+  ) {
+    const sub = getSubByToken(accessToken);
+    return this.missionService.failedMissionBySub({ sub, number });
+  }
 }
