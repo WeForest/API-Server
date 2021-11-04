f706f4f4fef295777252a34ecef714ab0f63d521
sunrabbit123 <qudwls185@naver.com>
2021-11-04 11:29:09 +0900

    add : profile swagger description

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -3,7 +3,8 @@ import { Controller, Get, Param, Patch, Headers, Body } from "@nestjs/common";
 import { ProfileService } from "./profile.service";
 import { ProfileInterface, UpdateProfileDataInterface } from "./profile.dto";
 import { getSubByToken } from "src/util/token";
-import { ApiTags } from "@nestjs/swagger";
+import { ApiOkResponse, ApiOperation, ApiTags } from "@nestjs/swagger";
+import { UserDTO } from "./profile.dto";
 
 @Controller("profile")
 @ApiTags("프로필 관련 API")
@@ -11,6 +12,8 @@ export class ProfileController {
   constructor(private readonly profileService: ProfileService) {}
 
   @Get(":nickname")
+  @ApiOperation({ summary: "프로필 조회", description: "프로필를 조회합니다." })
+  @ApiOkResponse({ description: "성공 시", type: UserDTO })
   async profileJoin(
     @Param("nickname") nickname: string
   ): Promise<ProfileInterface | { message: string }> {
@@ -24,6 +27,11 @@ export class ProfileController {
   }
 
   @Patch("update")
+  @ApiOperation({
+    summary: "프로필 업데이트",
+    description: "프로필을 업데이트합니다.",
+  })
+  @ApiOkResponse({ description: "성공 시", type: UserDTO })
   async profileUpdate(
     @Headers("authorization") accessToken: string,
     @Body() body: UpdateProfileDataInterface
