3e27f29bee0fdb65ef6c6e4c5d8c4a9a2f9f6425
sunrabbit123 <qudwls185@naver.com>
2021-11-08 12:04:55 +0900

    add : schema at swagger

--- src/main.ts
+++ src/main.ts
@@ -10,9 +10,7 @@ async function bootstrap() {
     .setVersion("1.0")
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup("api", app, document, {
-    swaggerOptions: { defaultModelsExpandDepth: -1 },
-  });
+  SwaggerModule.setup("api", app, document);
   await app.listen(process.env.PORT);
 }
 bootstrap();
