5aa3c1428165fc63770a5b60b9eea85c19309811
sunrabbit123 <qudwls185@naver.com>
2021-11-27 23:38:21 +0900

    add : id

--- src/group/group.service.ts
+++ src/group/group.service.ts
@@ -89,6 +89,7 @@ export class GroupService {
     return this.prisma.studyGroup.findMany({
       where: { name: { contains: keyword ?? "" } },
       select: {
+        id: true,
         name: true,
         description: true,
         tags: true,
